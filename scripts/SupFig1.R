22f43558ee6ea99c1c46493c93444e680f2f3083
rhong3 <michaelhong1995@gmail.com>
2022-02-09 12:09:19 -0500

    2.9.1

--- scripts/SupFig1.R
+++ scripts/SupFig1.R
@@ -39,3 +39,43 @@ grid.arrange(p1,p2,nrow=1)
 dev.off()
 
 
+# C3L-02557 story
+CNVH =read.csv("~/documents/confirm_CPTAC-UCEC/Results/X4CNVH_NL5/out/tSNE_P_N.csv")
+POLE = read.csv("~/documents/confirm_CPTAC-UCEC/Results/F2POLE_NL5/out/tSNE_P_N.csv")
+CNVH = CNVH[CNVH$slide == 'C3L-02557', c(9, 12, 13)]
+colnames(CNVH) = c("CNVH_score", "tsne1", "tsne2")
+CNVH$POLE_score = 0.0000
+POLE = POLE[POLE$slide == 'C3L-02557', c(9, 12, 13)]
+colnames(POLE) = c("POLE_score", "tsne1", "tsne2")
+POLE$CNVH_score = 0.0000
+DAT = rbind(CNVH, POLE)
+DAT = DAT[, c("CNVH_score", "POLE_score", "tsne1", "tsne2")]
+DAT$CNVH_score = as.numeric(DAT$CNVH_score)
+DAT$POLE_score = as.numeric(DAT$POLE_score)
+rownames(DAT) = 1:nrow(DAT)
+DAT = unique(DAT)
+
+library(ggplot2)
+library(gridExtra)
+p1 = ggplot(data=DAT,aes_string(x='tsne1',y='tsne2',col='CNVH_score'))+
+  scale_color_gradient2(high='red',mid='gray',low='steelblue',midpoint=0.5)+
+  geom_point(alpha=1, size=1)+ scale_shape(solid = TRUE)+
+  xlim(-60,60)+
+  ylim(-60,60)+
+  theme_bw() + theme(panel.border = element_blank(), panel.grid.major = element_blank(),
+                     panel.grid.minor = element_blank(), 
+                     axis.line = element_line(colour = "black"), legend.position='bottom')
+
+p2 = ggplot(data=DAT,aes_string(x='tsne1',y='tsne2',col='POLE_score'))+
+  scale_color_gradient2(high='red',mid='gray',low='steelblue',midpoint=0.5)+
+  geom_point(alpha=1, size=1)+ scale_shape(solid = TRUE)+
+  xlim(-60,60)+
+  ylim(-60,60)+
+  theme_bw() + theme(panel.border = element_blank(), panel.grid.major = element_blank(),
+                     panel.grid.minor = element_blank(), 
+                     axis.line = element_line(colour = "black"), legend.position='bottom')
+
+pdf(file='~/documents/confirm_CPTAC-UCEC/Figures/C3L-02557.pdf', width=14,height=7)
+
+grid.arrange(p1,p2,nrow=1)
+dev.off()
